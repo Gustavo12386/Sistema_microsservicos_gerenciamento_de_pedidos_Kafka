3f5be319471e9f023c5839e67c07b43627547fee
Gustavo12386 <gucalderaro@hotmail.com>
2025-08-14 11:28:36 -0300

    adicionando arquivos de order-service

---
+++ order-service/src/main/java/com/gustavo/order_service/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.gustavo.order_service.controller;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gustavo.base_domains.dto.Order;
+import com.gustavo.base_domains.dto.OrderEvent;
+import com.gustavo.order_service.kafka.OrderProducer;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+	private OrderProducer orderProducer;
+
+	public OrderController(OrderProducer orderProducer) {		
+		this.orderProducer = orderProducer;
+	}
+	
+	@PostMapping("/orders")
+	public String placeOrder(@RequestBody Order order) {
+		order.setOrderId(UUID.randomUUID().toString());
+		
+		OrderEvent orderEvent = new OrderEvent();
+		orderEvent.setStatus("PENDING");
+		orderEvent.setMessage("order status is in pending state");
+		orderEvent.setOrder(order);
+		
+		orderProducer.sendMessage(orderEvent);
+		
+		return "Order placed successfully...";
+	}
+}
